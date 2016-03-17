16d215fcd82db332c6ada2cb9f2bec4b2431cb93
Rahul Malhotra <rvhul.malhotra@gmail.com>
2016-03-17 14:52:39 +0530

    Student details view complete.

--- app/models/student.rb
+++ app/models/student.rb
@@ -22,6 +22,14 @@ class Student < ActiveRecord::Base
     MALE = {code: 1, label: "Male"}
     FEMALE = {code: 2, label: "Female"}
 
+    def self.label(code)
+      if code == MALE[:code]
+        return MALE[:label]
+      else
+        return FEMALE[:label]
+      end
+    end
+
     def self.all
     [MALE, FEMALE].collect{|hsh| OpenStruct.new(hsh)}
     end
