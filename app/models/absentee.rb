22a51786c9093120ee49b350364b074e85514ab2
Rahul Malhotra <rvhul.malhotra@gmail.com>
2016-03-21 20:23:59 +0530

    Mock SMS attendance notification sent.

--- app/models/absentee.rb
+++ app/models/absentee.rb
@@ -1,4 +1,16 @@
 class Absentee < ActiveRecord::Base
   belongs_to :student
   belongs_to :attendance_registry
+
+  after_create :send_absent_notification!
+  after_destroy :send_present_notification!
+
+private
+  def send_absent_notification!
+    puts ">>>>>> send sms to #{self.student.phone}"
+  end
+
+  def send_present_notification!
+    puts ">>>>>send present sms to #{self.student.phone}"
+  end
 end
